85c28bc2403fdfc52f7cfb1e888bfe6dad08a1bb
cihan yılmaz <cihan.yilmazz144@gmail.com>
2022-09-18 23:48:55 +0100

    added ETL pipeline

---
+++ python-etl-pipeline/etl.py
@@ -0,0 +1,40 @@
+#import needed libraries
+from sqlalchemy import create_engine
+import pandas as pd
+from sqlalchemy import inspect
+
+pwd = "123456"
+uid = "postgres"
+server = "localhost"
+
+#extract data from postgres
+def extract():
+    try:
+        engine = create_engine(f'postgresql://{uid}:{pwd}@{server}:5435/data-db')
+        insp = inspect(engine)
+        print(insp.get_table_names())
+        src_tables = insp.get_table_names()
+        for tbl in src_tables:
+            df = pd.read_sql_query(f'select * FROM {tbl}',engine)       
+            load(tbl,df)
+    except Exception as e:
+        print("Data extract error: " + str(e))
+
+#load data to postgres
+def load(tbl,df):
+    try:
+        rows_imported = 0
+        engine = create_engine(f'postgresql://{uid}:{pwd}@{server}:5435/destination-db')
+        print(f'importing rows {rows_imported} to {rows_imported + len(df)} for table {tbl}')
+        # save df to postgres
+        df.to_sql(f'stg_{tbl}', engine, if_exists='replace', index=False)
+        rows_imported += len(df)      
+        print("data imported successful")
+    except Exception as e:
+        print("Data load error: " + str(e))
+
+try:
+    #call extract function
+    extract()  
+except Exception as e:
+    print("Error while extracting data: " + str(e))
---
+++ python-etl-pipeline/readme.txt
@@ -0,0 +1,11 @@
+firtly you should run below commands 
+
+pip install sqlalchemy
+
+pip install pandas
+
+pip install psycopg2
+
+after installing libraries run below command
+
+python etl.py
